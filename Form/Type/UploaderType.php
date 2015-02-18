d5c0411f3d68ba7a9a4cfa55b8be412e477663b9
gus <tugui.lucian@gmail.com>
2015-02-18 18:22:34 +0100

    added new uploader template variables

--- Form/Type/UploaderType.php
+++ Form/Type/UploaderType.php
@@ -3,6 +3,8 @@
 namespace Gus\UploaderBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 
 /**
  * Class UploaderType
@@ -10,6 +12,30 @@ use Symfony\Component\Form\AbstractType;
  */
 class UploaderType extends AbstractType
 {
+    /**
+     * @var
+     */
+    private $formName;
+
+    /**
+     * @param $formName
+     */
+    public function __construct($formName)
+    {
+        $this->formName = $formName;
+    }
+
+    /**
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['gus_uploader_form_name'] = sprintf('%s[%s]', $view->parent->vars['full_name'], $this->formName);
+        $view->vars['gus_uploader_form_id'] = sprintf('%s_%s', $view->parent->vars['full_name'], $this->formName);
+    }
+
     /**
      * @return string
      */
